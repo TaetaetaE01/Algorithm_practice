b5dd111584da7c698c37a67d5b7aab4e8722d627
TaetaetaE01 <grgr589@mju.ac.kr>
2022-12-10 17:18:54 +0900

    Bronze_10818

---
+++ src/Bronze/Bronze_10818.java
@@ -0,0 +1,22 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bronze_10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int A[] = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        System.out.println(A[0] + " " + A[A.length - 1]);
+    }
+}
