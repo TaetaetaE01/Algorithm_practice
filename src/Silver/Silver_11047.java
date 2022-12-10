66908506c0abf206088b311ddaa06f29f5fb21c1
TaetaetaE01 <grgr589@mju.ac.kr>
2022-12-10 22:36:19 +0900

    Silver_11047

---
+++ src/Silver/Silver_11047.java
@@ -0,0 +1,29 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Silver_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int A[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        int coinCount = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (A[i] <= K) {
+                coinCount += K / A[i];
+                K = K % A[i];
+            }
+        }
+        System.out.println(coinCount);
+    }
+}
