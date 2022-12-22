f040024062b5445f9d3fa9b9e81ca3a48b4a8b51
TaetaetaE01 <grgr589@mju.ac.kr>
2022-12-22 22:37:59 +0900

    Silver_1003 fibonacci 수열 ㅜㅜㅜ dp 계산법

--- src/Silver/Silver_1003.java
+++ src/Silver/Silver_1003.java
@@ -1,37 +1,72 @@
 package Silver;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Silver_1003 {
+    static Integer[][] dp = new Integer[41][2];
 
-	static int countZ = 0;
-	static int countO = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
+        dp[0][0] = 1;    // N=0 일 때의 0 호출 횟수
+        dp[0][1] = 0;    // N=0 일 때의 1 호출 횟수
+        dp[1][0] = 0;    // N=1 일 때의 0 호출 횟수
+        dp[1][1] = 1;    // N=1 일 때의 1 호출 횟수
 
-		Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(br.readLine());
 
-		int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < n; i++) {
-			int t = sc.nextInt();
-			fibonacci(t);
-		}
-		for (int m = 0; m < n; m++) {
-			System.out.println(countZ + " " + countO);
-		}
-	}
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            fibonacci(N);
+            sb.append(dp[N][0] + " " + dp[N][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
 
-	public static int fibonacci(int t) {
+    static Integer[] fibonacci(int N) {
+        // N에 대한 0, 1의 호출 횟수가 없을 떄(탐색하지 않은 값일 때)
+        if (dp[N][0] == null || dp[N][1] == null) {
+            // 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
+            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+        }
+        // N에 대한 0과 1, 즉 [N][0]과 [N][1] 을 담고있는 [N]을 반환한다.
+        return dp[N];
 
-		if (t == 0) {
-			countZ++;
-			return 0;
-		} else if (t == 1) {
-			countO++;
-			return 1;
-		} else {
-			return fibonacci(t - 1) + fibonacci(t - 2);
-		}
-	}
+    }
+
+//    static int countZ = 0;
+//    static int countO = 0;
+//    static StringBuilder sb = new StringBuilder();
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int n = Integer.parseInt(br.readLine());
+//
+//        for (int i = 0; i < n; i++) {
+//            fibonacci(Integer.parseInt(br.readLine()));
+//            sb.append(countZ).append(" ").append(countO).append("\n");
+//            countZ = 0;
+//            countO = 0;
+//        }
+//        System.out.println(sb);
+//    }
+//
+//    public static void fibonacci(int n) {
+//        if (n == 0) {
+//            countZ++;
+//        } else if (n == 1) {
+//            countO++;
+//        } else if (n < 0) {
+//            return;
+//        } else {
+//            fibonacci(n - 1);
+//            fibonacci(n - 2);
+//        }
+//    }
 }
