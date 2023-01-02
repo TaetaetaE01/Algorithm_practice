401fa247c532643fe0354dd1b6c26d78e27c6a79
TaetaetaE01 <grgr589@mju.ac.kr>
2023-01-02 23:19:30 +0900

    Gold_7576 토마토 BFS로 익은 토마토 search후 땅에 점차 넓혀감

--- src/Gold/Gold_7576.java
+++ src/Gold/Gold_7576.java
@@ -3,24 +3,71 @@ package Gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Gold_7576 {
 
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
-    static boolean[][] visited;
     static int[][] A;
     static int M, N;
+    static Queue<int[]> queue = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         A = new int[M][N];
-        visited = new boolean[M][N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < N; j++) {
+                int input = Integer.parseInt(st.nextToken());
+                A[i][j] = input;
+            }
+        }
+
+        for (int x = 0; x < M; x++) {
+            for (int y = 0; y < N; y++) {
+                if (A[x][y] == 1) {
+                    queue.add(new int[]{x, y});
+                }
+            }
+        }
+        System.out.println(BFS());
+    }
+
+    private static int BFS() {
+        while (!queue.isEmpty()) {
+            int now[] = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
+
+                if (x >= 0 && y >= 0 && x < M && y < N) {
+                    if (A[x][y] == 0) {
+                        A[x][y] = A[now[0]][now[1]] + 1;
+                        queue.add(new int[]{x, y});
+                    }
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (A[i][j] == 0) {
+                    return -1;
+                } else {
+                    max = Math.max(max, A[i][j]);
+                }
+            }
+        }
+        return max - 1;
     }
 }
