afd7b9d51a1ac086e4161ddc9b3597e83743b2e9
TaetaetaE01 <grgr589@mju.ac.kr>
2023-02-14 22:39:44 +0900

    Gold_2251 물통문제 이해불가

--- src/Gold/Gold_2251.java
+++ src/Gold/Gold_2251.java
@@ -29,7 +29,7 @@ public class Gold_2251 {
         BFS();
         for (int i = 0; i < answer.length; i++) {
             if (answer[i]) {
-                System.out.println(i + " ");
+                System.out.print(i + " ");
             }
         }
     }
@@ -43,35 +43,35 @@ public class Gold_2251 {
             AB p = queue.poll();
             int A = p.A;
             int B = p.B;
-            int C = now[2] - A - B;
-            for (int k = 0; k < 6; k++) {
+            int C = now[2] - A - B; // C는 전체 물의 양에서 A와 B를 빼준 것
+            for (int k = 0; k < 6; k++) { // A->B, A->C, B->A, B->C, C->A, C->B 6개의 케이스로 이동
                 int[] next = {A, B, C};
-                next[Receiver[k]] = 0;
-                // 물이 넘칠 때
-                if (next[Receiver[k]] > now[Receiver[k]]) {
+                next[Receiver[k]] += next[Sender[k]];
+                next[Sender[k]] = 0;
+                if (next[Receiver[k]] > now[Receiver[k]]) { // 대상 물통의 용량보다 물이 많아 넘칠 때
+                    // 초과하는 만큼 다시 이전 물통에 넣어줌
                     next[Sender[k]] = next[Receiver[k]] - now[Receiver[k]];
-                    next[Receiver[k]] = now[Receiver[k]];
+                    next[Receiver[k]] = now[Receiver[k]]; // 대상 물통은 최대로 채워줌
                 }
-                if (!visited[next[0]][next[1]]) {
+                if (!visited[next[0]][next[1]]) { // A와 B의 물의 양을 통하여 방문 배열 체크
                     visited[next[0]][next[1]] = true;
                     queue.add(new AB(next[0], next[1]));
-                    if (next[0] == 0) {
+                    if (next[0] == 0) {  // A의 물의 양이 0일때 C의 물의 무게를 정답 변수에 저장
                         answer[next[2]] = true;
                     }
                 }
             }
         }
-
     }
+}
 
-    static class AB {
-        int A;
-        int B;
+class AB {
+    int A;
+    int B;
 
-        public AB(int A, int B) {
-            this.A = A;
-            this.B = B;
-        }
+    public AB(int A, int B) {
+        this.A = A;
+        this.B = B;
     }
 }
 
